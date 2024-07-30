434f4bc3ded152436b378bb26be528ff5f8e50ca
wanjiru-njuguna <missnjuguna19@gmail.com>
2024-07-30 23:46:59 +0300

    added sqlite option for development.

--- rhs_soccer/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ rhs_soccer/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -16,13 +16,19 @@ def _update_or_create_site_with_sequence(site_model, connection, domain, name):
             "name": name,
         },
     )
-    if created:
+    # Determine the database vendor
+    if connection.vendor == 'sqlite':
+        # Logic for SQLite
+        site.domain = "example.com"
+        site.name = "Example Site"
+        #if created:
         # We provided the ID explicitly when creating the Site entry, therefore the DB
         # sequence to auto-generate them wasn't used and is now out of sync. If we
         # don't do anything, we'll get a unique constraint violation the next time a
         # site is created.
         # To avoid this, we need to manually update DB sequence and make sure it's
         # greater than the maximum value.
+    else:
         max_id = site_model.objects.order_by("-id").first().id
         with connection.cursor() as cursor:
             cursor.execute("SELECT last_value from django_site_id_seq")
