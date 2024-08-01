93cbd5eba435a623a3790a22a11c437d61e7cd92
wanjiru-njuguna <missnjuguna19@gmail.com>
2024-08-01 21:09:27 +0300

    added django all-auth templates.

--- config/settings/base.py
+++ config/settings/base.py
@@ -210,7 +210,8 @@ TEMPLATES = [
         # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         # https://docs.djangoproject.com/en/dev/ref/settings/#dirs
-        "DIRS": [str(APPS_DIR / "templates")],
+        #"DIRS": [str(APPS_DIR / "templates")],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         # https://docs.djangoproject.com/en/dev/ref/settings/#app-dirs
         "APP_DIRS": True,
         "OPTIONS": {
--- rhs_soccer/users/forms.py
+++ rhs_soccer/users/forms.py
@@ -36,6 +36,13 @@ class UserSignupForm(SignupForm):
     Default fields will be added automatically.
     Check UserSocialSignupForm for accounts created from social.
     """
+    first_name = forms.CharField(max_length=30, label='First Name')
+    last_name = forms.CharField(max_length=30, label='Last Name')
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
 
 
 class UserSocialSignupForm(SocialSignupForm):
